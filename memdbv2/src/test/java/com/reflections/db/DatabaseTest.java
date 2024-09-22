1524bd8f5c2fc8e4c1946db6c71d1102325d28f2
Kamalesh S <dalinar@Kamaleshs-MacBook-Air.local>
2024-09-22 16:21:21 +0530

    wrote a simple test to test commit and rollback

--- memdbv2/src/test/java/com/reflections/db/DatabaseTest.java
+++ memdbv2/src/test/java/com/reflections/db/DatabaseTest.java
@@ -1,5 +1,6 @@
 package com.reflections.db;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DatabaseTest {
@@ -15,28 +16,28 @@ public class DatabaseTest {
         usersTable.insert("1", "Alice", transaction);
         usersTable.insert("2", "Bob", transaction);
 
-        System.out.println("Before Commit:");
-        System.out.println("User 1: " + usersTable.read("1", transaction));
-        System.out.println("User 2: " + usersTable.read("2", transaction));
+        // Before Commit
+        Assertions.assertEquals("Alice", usersTable.read("1", transaction));
+        Assertions.assertEquals("Bob", usersTable.read("2", transaction));
 
         transaction.commit();
 
-        System.out.println("After Commit:");
-        System.out.println("User 1: " + usersTable.read("1", null));
-        System.out.println("User 2: " + usersTable.read("2", null));
+        // After Commit
+        Assertions.assertEquals("Alice", usersTable.read("1", null));
+        Assertions.assertEquals("Bob", usersTable.read("2", null));
 
         Transaction transaction2 = new Transaction();
         usersTable.update("1", "Alice Updated", transaction2);
         usersTable.delete("2", transaction2);
 
-        System.out.println("Before Rollback:");
-        System.out.println("User 1: " + usersTable.read("1", transaction2));
-        System.out.println("User 2: " + usersTable.read("2", transaction2));
+        // Before Rollback
+        Assertions.assertEquals("Alice Updated", usersTable.read("1", transaction2));
+        Assertions.assertNull(usersTable.read("2", transaction2));
 
         transaction2.rollback();
 
-        System.out.println("After Rollback:");
-        System.out.println("User 1: " + usersTable.read("1", null));
-        System.out.println("User 2: " + usersTable.read("2", null));
+        // After Rollback
+        Assertions.assertEquals("Alice", usersTable.read("1", null));
+        Assertions.assertEquals("Bob", usersTable.read("2", null));
     }
 }
